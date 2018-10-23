2e1e0d54e4d75e441b9dbe966b81495943465687
Divaweb <lanittad@hotmail.com>
2018-10-22 17:20:42 -0700

    error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
-  def create
+  def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new
+    if @item.save
+      flash[:success] = "You have added the To Do to your list."
+      redirect_to @item
+    else
+      render 'create'
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   get 'items/create'
   resources :users do
-    resources :items, only: [:create]
+    resources :items
   end
   devise_for :users
-  
+
 
   root 'users#show'
 end

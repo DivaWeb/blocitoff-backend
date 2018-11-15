cb5c5cf0167b12c7235116ede1c177a04c238e1a
Divaweb <lanittad@hotmail.com>
2018-11-14 20:46:45 -0700

    seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_221902) do
+ActiveRecord::Schema.define(version: 2018_10_21_235000) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2018_10_16_221902) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+50.times do
+  User.create!(
+
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.unique.password(8)
+
+  )
+end
+
+users = User.all
+
+50.times do
+  Item.create!(
+        user: users.sample,
+        name: Faker::Lorem.characters(6)
+
+  )
+end
+
+puts "seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} todos created"

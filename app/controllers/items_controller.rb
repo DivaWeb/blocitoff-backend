e933dc35ab243dfa38e65a6f44dcdc8ce9606725
Divaweb <lanittad@hotmail.com>
2018-11-20 22:10:53 -0700

    working ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,6 @@ class ItemsController < ApplicationController
 respond_to :html, :js, only: :destroy
 
 
-  def index
-    #user = User.find(params[:user_id])
-    @items = item.all
-  end
 
   def new
     @item = Item.new
@@ -23,17 +19,20 @@ respond_to :html, :js, only: :destroy
 
   def destroy
     @item = Item.find(params[:id])
-     @item.destroy
-
-
+    @item.destroy
     respond_to do |format|
       format.html { redirect_to root, notice: "Item was successfully destroyed."}
-      format.json { head :no_content }
-      format.js { render :layout => false }
+      #format.json { head :no_content }
+      format.js
     end
   end
 
   private
+
+  def all_items
+  @items = Item.all
+  @user = current_user
+end
 
   def item_params
     params.require(:item).permit(:name, :user_id)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @item = Item.new
+    @items = Item.all
   end
 end
